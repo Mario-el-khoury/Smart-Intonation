97f38d85b0752f51ed3b147008a3e3980f9c3fad
mario-el-khoury <2491130e@student.gla.ac.uk>
2021-04-16 08:49:50 +0100

    Additional Documentation

--- src/window.h
+++ src/window.h
@@ -12,115 +12,101 @@
 #include <fftw3.h>
 #include <QLineEdit>
 
-// {a=440.00, b=493.88, c=523.25, d=587.33, e=659.25, f=698.46, g=783.99};
+//  Major Scales {a=440.00, b=493.88, c=523.25, d=587.33, e=659.25, f=698.46, g=783.99};
 
 /** class definition 'Window' where every tone is generated any recieved using fft   */
 
 class Window : public QWidget
 {
 	// must include the Q_OBJECT macro for for the Qt signals/slots framework to work with this class
+	/** Q_OBJECT macro for for the Qt signals/slots framework */
 	Q_OBJECT
 
 public:
-	Window(); // default constructor - called when a Window is declared without arguments
-
+    /** Default constructor - called when a Window is declared without arguments */
+	Window(); 
+    /** Default deconstructor */
 	~Window();
 
 
-
+/** public  functions for the window class */
 public slots:
 	
 	/** function to play the c scale video */ 
 		void playslot();           
-
     /** Function to handle proper shutdown of the window */
 		void quitApp();   
-
     /** function that pause the video */
 		void pauseslot();    
-
 	/** function that resume the video */	 
 		void resumeslot();
-
     /** function that starts taking microphone input  */
 		void AudioRecorderSlot();
-
     /** function that genrate random tone to test the user knowledge  */
 		void TestingSlot();
-
     /** function that exit(back) the video or any other sub-phase */
 		void exitslot();
-
 	/** function that stop audio record */	
 		void stopRecording();
-
     /** function that confirm that Do is pressed */
 		void DoPressedSlot();
-
 	/** function that confirm that Re is pressed */	
 		void RePressedSlot();
-
 	/* function that confirm that Mi is pressed */	
 		void MiPressedSlot();
-
 	/** function that confirm that Fa is pressed */	
 		void FaPressedSlot();
-
 	/** function that confirm that So is pressed */	
 		void SoPressedSlot();
-
 	/** function that confirm that La is pressed */	
 		void LaPressedSlot();
-	
 	/** function that confirm that Si is pressed */
 		void SiPressedSlot();
-
     /** function that handle the state of the microphone */
 		void handleStateChanged(QAudio::State newState);
-
     /** function that read mic data */
 		void readMicrophone();
-
+    /** function that exit and stop the audio */
 		void audioexit();
 
-// internal variables for the window class
+/** internal variables for the window class */
 private:
 
     //! Clickable button pointers.
     /*! Pointers to QPushButtons to allow user to press any button on the window  */
- 	//** intialisation of pushbutton1 */
+ 	//** Declaration of pushbutton1 */
 	QPushButton *pushbutton1;
-	//** intialisation of pushbutton2 */
+	//** Declaration of pushbutton2 */
 	QPushButton *pushbutton2;
-	//** intialisation of pushbutton3 */
+	//** Declaration of pushbutton3 */
 	QPushButton *pushbutton3;
-	//** intialisation of quitbutton */
+	//** Declaration of quitbutton */
     QPushButton *quitbutton;
-	//** intialisation of  stopbutton */
+	//** Declaration of  stopbutton */
 	QPushButton *stopbutton;
-	//** intialisation of resumebutton */
+	//** Declaration of resumebutton */
 	QPushButton *resumebutton;
-	//** intialisation of exitbutton */
+	//** Declaration of exitbutton */
 	QPushButton *exitbutton;
-	//** intialisation of audiostopbutton */
+	//** Declaration of audiostopbutton */
 	QPushButton *audiostopbutton;
-	//** intialisation of dobutton */
+	//** Declaration of dobutton */
 	QPushButton *dobutton;
-	//** intialisation of rebutton */
+	//** Declaration of rebutton */
 	QPushButton *rebutton;
-	//** intialisation of mibutton */
+	//** Declaration of mibutton */
 	QPushButton *mibutton;
-	//** intialisation of fabutton */
+	//** Declaration of fabutton */
 	QPushButton *fabutton;
-	//** intialisation of sobutton */
+	//** Declaration of sobutton */
 	QPushButton *sobutton;
-	//** intialisation of labutton */
+	//** Declaration of labutton */
 	QPushButton *labutton;
-	//** intialisation of sibutton */
+	//** Declaration of sibutton */
 	QPushButton *sibutton;
-	//** intialisation of feedbackbutton */
+	//** Declaration of feedbackbutton */
  	QPushButton *feedbackbutton;
-	 //** intialisation of text , text2, and text 3 */
+	 //** Declaration of text , text2, and text 3 */
 	QLineEdit *text, *text2, *text3;
 
 	/** Pointer to access recorded audio*/
@@ -134,45 +120,39 @@ private:
 	/** Pointer to change audio format*/
 	QAudioFormat format;
      
-
 	// layout elements from Qt itself http://qt-project.org/doc/qt-4.8/classes.html
-	QVBoxLayout  *vLayout;  // vertical layout
-	QHBoxLayout  *hLayout;  // horizontal layout
+    /** Vertical layout */
+	QVBoxLayout  *vLayout;  
+	/** Horizontal layout*/
+	QHBoxLayout  *hLayout;  
 
     /** pointer to access fftinputbuffer */
 	double* fftinputbuffer;
-
-    /** variable to save the fftbuffsize */
+    /** Declaration of a variable to save the fftbuffsize */
 	int fftbuffsize;
-
-    /** pointer to access fftoutputbuffer */
+    /** Declaration of a pointer to access fftoutputbuffer */
 	fftw_complex* fftoutputbuffer;
-
-	/** function that execute the fft plan */
+	/** Declaration of a function that execute the fft plan */
 	fftw_plan plan;
 
     /** variable to intialise count as 0 */
 	int count=0;
-
-    /** variable to intialise the value of sampleRate */
+    /**  Intialise sampleRate */
 	double sampleRate = 48000; 
-	
-
-    /** variable to intialise the buffer time in ms  */
+    /** Intialise  buffer time in ms  */
 	int bufferTime = 600.0; 
-
 	/** variable to store peakHeartz values */
 	double peakHertz;
-    /** variable to intialise peakHeartzScale and define which scale the user is using */
+    /** Intialise peakHeartzScale and define which scale the user is using */
 	double peakHertzScale =523.25;
+
 	/** Intialise the lowest frequency detected by the software */
     double lowestFrequency = 100 ;
 	/** Intialise the highest frequency detected by the software */
 	double highestFrequency = 1000;
-    /** pointer to access readmicarray values */
+    /** Pointer to access readmicarray values */
 	QByteArray* readmicarray;
-
-	/** vpointer to access readmic values */
+	/** Pointer to access readmic values */
 	QBuffer* readMic;
 
 
