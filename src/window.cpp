32e65e96317a223674e3d7adfdc5dba67493b5b9
mario-el-khoury <2491130e@student.gla.ac.uk>
2021-04-17 09:15:31 +0100

    Delete Usless lines

--- src/window.cpp
+++ src/window.cpp
@@ -682,10 +682,6 @@ void Window::TestingSlot()
 	sibutton->show();
    	feedbackbutton->setText(tr("feedback"));
 	feedbackbutton->show();
-	//std::random_device dev;
-    //std::mt19937 rng(dev());
-  //  std::uniform_int_distribution<std::mt19937::result_type> dist7(1,7); // distribution in range [1, 6]
-    
 
     srand((unsigned) time(0));
     int randomNumber;
